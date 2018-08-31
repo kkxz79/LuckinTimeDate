124db618ee4788c471cd86bd9e642c640e3b4884
kkxz79 <kkxz79@126.com>
2018-08-31 16:38:53 +0800

    提交修改

--- LuckinTimeDate/LuckinBaseManager.h
+++ LuckinTimeDate/LuckinBaseManager.h
@@ -4,10 +4,10 @@
 //
 //  Created by kkxz on 2018/8/31.
 //  Copyright © 2018年 kkxz. All rights reserved.
-//
+//时间转换基类
 
 #import <Foundation/Foundation.h>
 
 @interface LuckinBaseManager : NSObject
-
+- (NSString *)changeToStandardTime:(NSTimeInterval)timeInterval;
 @end
--- LuckinTimeDate/LuckinBaseManager.m
+++ LuckinTimeDate/LuckinBaseManager.m
@@ -9,5 +9,12 @@
 #import "LuckinBaseManager.h"
 
 @implementation LuckinBaseManager
+- (NSString *)changeToStandardTime:(NSTimeInterval)timeInterval
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSString *time = [dateFormatter stringFromDate:[NSDate dateWithTimeIntervalSince1970:timeInterval]];
+    return time;
+}
 
 @end
